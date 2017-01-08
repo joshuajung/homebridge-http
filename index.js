74f4fcd948d752c7071edb3b3eb5edaeed8746ae
Joshua Jung <mail@joshua-jung.com>
2017-01-08 16:09:28 +0100

    Dev

--- index.js
+++ index.js
@@ -105,7 +105,8 @@ JositorAccessory.prototype = {
 		.setCharacteristic(Characteristic.SerialNumber, "001");
 
 		this.garageService = new Service.GarageDoorOpener(this.name);
-		this.garageService.getCharacteristic(Characteristic.TargetDoorState).on('set', this.setGarageState.bind(this));
+		this.garageService.getCharacteristic(Characteristic.TargetDoorState).on('set', this.setTargetGarageState.bind(this));
+		this.garageService.getCharacteristic(Characteristic.CurrentDoorState).on('set', this.setCurrentGarageState.bind(this));
 		this.garageService.getCharacteristic(Characteristic.CurrentDoorState).on('get', this.getGarageState.bind(this));
 
 		return [this.garageService];
